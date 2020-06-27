c98a49a38f6cd93793b0262e9a848b53aeb6de35
Aligohar101 <aligoharkhokhar@gmail.com>
2020-06-27 19:47:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function getNumber(num)
+{
+    console.log(num)
+    var inpt = document.getElementById("inpt");
+    inpt.value +=num;
+}
+function clearinput()
+{
+    var inpt = document.getElementById("inpt");
+    inpt.value =""
+}
+function showresult()
+{
+    var inpt =document.getElementById('inpt');
+    inpt.value=eval(inpt.value)
+
+}
+function persentage() {
+    var inpt=document.getElementById('inpt')
+    inpt.value= Math.pow()
+    
+}
